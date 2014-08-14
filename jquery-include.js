9c9cc6f9d0fb64943d619de3f2c9c143fc8b3e6c
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-08-14 17:46:46 +0100

    Expand settings, define loadScripts and define final return call

--- jquery-include.js
+++ jquery-include.js
@@ -13,11 +13,45 @@ $.fn.include = function(options) {
   * @type {Object}
   */
   var settings = $.extend({
-    // ...
+    scripts: [],
+    success : function () {
+      // ...
+    },
+    error: function () {
+      // ...
+    },
+    complete : function() {
+      // ...
+    }
   }, options);
 
+  /**
+  * Recursively loads an array of scripts in order given
+  *
+  * @method loadScripts
+  * @param {Array} scripts URLs of the scripts to load
+  */
+  var loadScripts = function(scripts) {
+    if (scripts.length) {
+      var url = scripts.shift();
+
+      $.ajax({
+        url: url,
+        dataType: 'script',
+        success: function() {
+          loadScripts(scripts);
+        },
+        error: function() {
+          // ...
+        },
+      });
+    } else {
+      settings.complete();
+    }
+  };
+
   return this.each(function() {
-    // ...
+    loadScripts(settings.scripts);
   });
 };
 })(jQuery, window, document);
